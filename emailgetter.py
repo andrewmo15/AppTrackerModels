201c6a62bc53058cfd6caee42d75afd842f9ff6a
Alik-da-Geek <aliksemelianov@gmail.com>
2022-12-23 16:44:41 -0500

    Fixed email getter bugs

--- emailgetter.py
+++ emailgetter.py
@@ -6,15 +6,18 @@ import csv
 from bs4 import BeautifulSoup
 
  
-# user = 'aliksemelianov@gmail.com'
-# password = 'xqjybjodcaqktkcl'
-user = 'andrewmoasdf@gmail.com'
-password = 'rwkixnairelyitli'
+user = 'aliksemelianov@gmail.com'
+password = 'xqjybjodcaqktkcl'
+# user = 'andrewmoasdf@gmail.com'
+# password = 'rwkixnairelyitli'
 imap_url = 'imap.gmail.com'
 mail = imaplib.IMAP4_SSL(imap_url)
 mail.login(user, password)
 mail.select('Inbox')
 _, selected_mails = mail.search(None, 'ALL')
+
+selected_mails = selected_mails[0].split()
+selected_mails.reverse()
 
 def getTextFromHTML(html):
     soup = BeautifulSoup(html, features="html.parser")
@@ -32,7 +35,7 @@ def getTextFromHTML(html):
 
 emails = []
 counter = 0
-for num in selected_mails[0].split():
+for num in selected_mails:
     _, data = mail.fetch(num , '(RFC822)')
     _, bytes_data = data[0]
     counter += 1
@@ -40,8 +43,8 @@ for num in selected_mails[0].split():
     email_message = email.message_from_bytes(bytes_data)
     data = {}
     data["subject"] = email_message["subject"]
-    data["to"] = email_message["to"]
-    data["from"] = email_message["from"]
+    data["to"] = str(email_message["to"])
+    data["from"] = str(email_message["from"])
     data["date"] = email_message["date"]
     if data["subject"] == "" or data["to"] == "" or data["from"] == "" or data["date"] == "" or user in data["from"]:
         continue
@@ -50,13 +53,19 @@ for num in selected_mails[0].split():
     for part in email_message.walk():
         if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
             message = part.get_payload(decode=True)
+            # decode using correct charset
             try:
-                text = message.decode()
+                text = message.decode('utf-8', 'ignore')
             except:
-                text = message.decode("cp1252")
+                text = message.decode("cp1252", 'ignore')
+            # convert HTML to plain text
             if part.get_content_type()=="text/html":
                 text = getTextFromHTML(text)
-            data["body"] = " ".join(text.split())
+            # remove links
+            words = text.split()
+            for i, word in enumerate(words):
+                if "http" in word: del words[i]
+            data["body"] = " ".join(words)
             break
     if data["body"]=="":
         continue
@@ -64,6 +73,8 @@ for num in selected_mails[0].split():
     data["status"] = ""
     data["company"] = ""
     emails.append(data)
+    if counter == 2000:
+        break
 
 with open('emaildata.csv', 'w', encoding='UTF8') as f:
     header = ['subject', 'to', 'from', 'date', 'body', "status", "company"]
